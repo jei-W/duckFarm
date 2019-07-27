421f1e36fdd9362d93b6e9ea63a25977863f2167
beans907 <kyou8274@gmail.com>
2019-07-27 15:49:27 +0900

    no message

---
+++ DuckFarm/Assets/Scripts/Duck.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : ObjectBase
+{
+    enum GrowthLevel { egg, duckling, adult }
+    enum WorkState { idle, sleep, work }
+    bool male;
+    int age = 0;
+
+    //void 건물에 들어갈 수 있는지 물어본다
+}
